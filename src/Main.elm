2c534271c98c0a66c1d3213fab6e6372cd84e7c1
sairim <spiri@protonmail.com>
2019-08-19 10:43:41 +0300

    wire up ui to displaying door state

--- src/Main.elm
+++ src/Main.elm
@@ -1,5 +1,11 @@
-module Main exposing (..)
-import Html exposing (..)
+module Main exposing (AlarmState(..), DoorState(..), Model(..), Msg(..), initialModel, main, update, view)
+
+import Browser
+import Html exposing (Html, div, h1, img, p, text)
+import Html.Attributes exposing (class)
+import View.Alarm as Alarm exposing (armedAlarm, disarmedAlarm, triggeredAlarm)
+import View.Door as Door exposing (closedDoor, lockedDoor, openDoor)
+
 
 
 -- MODEL
@@ -17,6 +23,10 @@ type AlarmState
   = Armed
   | Disarmed
   | Triggered
+
+initialModel : Model
+initialModel =
+  DisplayingRoom Closed Armed
 
 -- UPDATE
 
@@ -73,3 +83,34 @@ update msg model =
 failure message =
   div []
     [ p [] [ text message ] ]
+
+view : Model -> Html Msg
+view model =
+  case model of
+    Failure message ->
+      failure message
+
+    DisplayingRoom doorState alarmState ->
+      div
+        []
+        [ div
+          [ class "doorPanel" ]
+          [ case doorState of
+            Opened ->
+              openDoor Close
+            Closed ->
+              closedDoor Open Lock
+            Locked ->
+              lockedDoor Unlock
+          ]
+        , div
+          [ class "alarmPanel" ]
+          [ case alarmState of
+            Armed ->
+              armedAlarm Disarm (doorState /= Opened)
+            Disarmed ->
+              disarmedAlarm Arm (doorState /= Opened)
+            Triggered ->
+              triggeredAlarm Disarm (doorState /= Opened)
+          ]
+        ]
